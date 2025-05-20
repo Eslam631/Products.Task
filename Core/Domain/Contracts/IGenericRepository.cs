24e930b8f7a3dab20fac2a8acc3cef26a5130f72
Eslam631 <aslamterk784@gmail.com>
2025-05-20 20:15:48 +0300

    Repository

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+public interface IGenericRepository<T> where T:BaseEntity
+    {
+
+        public IEnumerable<T> GetAll();
+
+
+    }
+}
---
+++ Infrastructure/Persistence/Repository/GenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Contracts;
+using Domain.Modules;
+using Persistence.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    public class GenericRepository<T>(ApplicationDbContext _dbContext) : IGenericRepository<T> where T : BaseEntity
+    {
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList(); 
+        }
+    }
+
+}
